a5c8a87d40631d955b7b2c55579257cbbd7c9ce7
ostandrey <ost.and.g@gmail.com>
2023-09-05 21:18:08 +0300

    fix styles of main page

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import { Box } from '@common';
+
 import { Header } from '../Header/Header';
 import { Sidebar } from '../Sidebar/Sidebar';
 
 export const BaseLayout = () => (
-  <>
+  <Box className='flex flex-row'>
     <Sidebar />
-    <Header />
-    <Outlet />
-  </>
+    <Box className='relative'>
+      <Header />
+      <Outlet />
+    </Box>
+  </Box>
 );
