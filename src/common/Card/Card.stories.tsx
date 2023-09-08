4621cd691f32786dab8075cd5bf7d9ab70846e12
ostandrey <ost.and.g@gmail.com>
2023-09-08 18:35:21 +0300

    create stories for Card

---
+++ src/common/Card/Card.stories.tsx
@@ -0,0 +1,21 @@
+import { Card } from '@common';
+import song from '@static/images/song.jpg';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CardProps } from '../../../@types/card';
+
+const meta: Meta<CardProps> = {
+  title: 'Card',
+  component: Card,
+  tags: ['autodoc']
+};
+
+export default meta;
+
+export const BaseCard: StoryObj<CardProps> = {
+  args: {
+    image: `${song}`,
+    title: 'Title',
+    description: 'Description'
+  }
+};
