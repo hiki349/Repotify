20c42b1d222d86be29259f9342d51cc82e83e680
ostandrey <ost.and.g@gmail.com>
2023-09-11 17:36:24 +0300

    create player

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,3 +2,4 @@ export * from './Box/Box';
 export * from './Button/Button';
 export * from './Card/Card';
 export * from './CustomLink/CustomLink';
+export * from './Typography/Typography';
--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,16 +1,21 @@
+import {} from 'module';
 import { Outlet } from 'react-router-dom';
 
 import { Box } from '@common';
 
 import { Header } from '../Header/Header';
+import { Player } from '../Player/Player';
 import { Sidebar } from '../Sidebar/Sidebar';
 
 export const BaseLayout = () => (
-  <Box className='flex flex-row'>
-    <Sidebar />
-    <Box className='relative'>
-      <Header />
-      <Outlet />
+  <>
+    <Box className='flex h-[calc(100%-80px)] flex-row'>
+      <Sidebar />
+      <Box className='relative'>
+        <Header />
+        <Outlet />
+      </Box>
     </Box>
-  </Box>
+    <Player />
+  </>
 );
