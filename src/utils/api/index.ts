2aa3e9858cc57ed9540a114e46a7c14d52b2147e
ostandrey <ost.and.g@gmail.com>
2023-09-12 14:17:57 +0300

    export response

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,4 +1,5 @@
 export * from './axiosConfig';
+export * from './response/Categories/getSeveralBrowseCategories';
 export * from './response/Playlist/getCategoryPlaylist';
 export * from './response/Playlist/getCurrentUsersPlaylist';
 export * from './response/Track/getArtist';
