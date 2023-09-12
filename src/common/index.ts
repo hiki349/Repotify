6811cbea9be2ca7a13607d9d5c89d28139c6ea9e
ostandrey <ost.and.g@gmail.com>
2023-09-12 14:03:26 +0300

    create Input

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,4 +2,5 @@ export * from './Box/Box';
 export * from './Button/Button';
 export * from './Card/Card';
 export * from './CustomLink/CustomLink';
+export * from './Input/Input';
 export * from './Typography/Typography';
